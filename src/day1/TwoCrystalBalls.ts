531aaa17981635c8ea96263e2080f1615fa57025
young.bae <young.bae@woven-planet.global>
2023-01-15 16:06:12 +0900

    TwoCrystalBalls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,27 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    let squareLootOfLength = Math.floor(Math.sqrt(breaks.length));
 
+    if (breaks[0] === true) {
+        return 0;
+    }
+
+    while (squareLootOfLength > 0) {
+        for (
+            let i = squareLootOfLength;
+            i < breaks.length;
+            i += squareLootOfLength
+        ) {
+            if (breaks[i]) {
+                for (let j = i - 1; j > 0; j--) {
+                    if (!breaks[j]) {
+                        return j + 1;
+                    }
+                }
+            }
+        }
+
+        squareLootOfLength--;
+    }
+
+    return -1;
 }
