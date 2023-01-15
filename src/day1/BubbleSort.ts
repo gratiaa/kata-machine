fb9a55f5487adc6c80536b26ad6ed844d3021315
young.bae <young.bae@woven-planet.global>
2023-01-15 21:36:17 +0900

    Bubblesort: don't need to include i

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,6 +1,6 @@
 export default function bubble_sort(arr: number[]): void {
     for (let i = arr.length - 1; i > 1; i--) {
-        for (let j = 0; j <= i; j++) {
+        for (let j = 0; j < i; j++) {
             if (arr[j] > arr[j + 1]) {
                 let temp = arr[j + 1];
                 arr[j + 1] = arr[j];
