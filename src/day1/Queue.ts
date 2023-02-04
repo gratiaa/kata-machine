0f39bd907e4a18b7bc51b7685e1273a9e89687a9
young.bae <young.bae@woven-planet.global>
2023-02-04 21:16:05 +0900

    Complete: Queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,57 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+};
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
+        const newNode = {
+            value: item,
+        };
 
-}
+        if (this.tail) {
+            const tail = this.tail;
+            tail.next = newNode;
+            this.tail = newNode;
+        } else {
+            this.head = this.tail = newNode;
+        }
+
+        this.length++;
+
+        console.log(this.head?.value, this.tail?.value);
+    }
     deque(): T | undefined {
+        const firstNode = this.head;
 
-}
+        if (!firstNode) {
+            return undefined;
+        }
+
+        if (this.head === this.tail) {
+            this.head = this.tail = undefined;
+        } else {
+            this.head = firstNode.next;
+        }
+
+        this.length--;
+
+        return firstNode.value;
+    }
     peek(): T | undefined {
+        if (this.head) {
+            return this.head.value;
+        }
 
-}
+        return undefined;
+    }
 }
