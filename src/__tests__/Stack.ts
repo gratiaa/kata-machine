7be0eb3abc213298c691e4d59db49fd9f348d88e
young.bae <young.bae@woven-planet.global>
2023-02-04 21:44:57 +0900

    Stack

--- src/__tests__/Stack.ts
+++ src/__tests__/Stack.ts
@@ -16,6 +16,7 @@ test("stack", function () {
     expect(list.peek()).toEqual(5);
     expect(list.pop()).toEqual(5);
     expect(list.pop()).toEqual(undefined);
+    expect(list.length).toEqual(0);
 
     // just wanted to make sure that I could not blow up myself when i remove
     // everything
--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,43 @@
+type Node<T> = {
+    value: T;
+    prev?: Node<T>;
+};
+
 export default class Stack<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = undefined;
     }
 
     push(item: T): void {
+        const newNode = {
+            value: item,
+        } as Node<T>;
 
-}
+        if (this.head) {
+            const currentHeadNode = this.head;
+            newNode.prev = currentHeadNode;
+            this.head = newNode;
+        } else {
+            this.head = newNode;
+        }
+
+        this.length++;
+    }
     pop(): T | undefined {
+        if (this.head) {
+            const currentHeadNode = this.head;
+            this.head = currentHeadNode.prev;
+            this.length--;
+            return currentHeadNode.value;
+        }
 
-}
+        return undefined;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
